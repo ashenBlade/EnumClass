9c0f30c0afa4e2ff16ed145184e358763c5b58c9
ash.blade <ashen.blade.dev@gmail.com>
2023-05-08 09:28:23 +0300

    Add sample with nested enum in partial class

---
+++ samples/EnumClass.NestedEnum/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EnumClass.NestedEnum;
+
+Console.WriteLine("Enter staff name: ");
+
+while (Console.ReadLine() is var name)
+{
+    if (Staff.TryParse(name, out var staff))
+    {
+        staff!.Speak();
+        break;
+    }
+
+    Console.WriteLine("Could not recognize staff type. \nEnter again: ");
+}
---
+++ samples/EnumClass.NestedEnum/Staff.cs
@@ -0,0 +1,64 @@
+using EnumClass.Attributes;
+
+namespace EnumClass.NestedEnum;
+
+public partial class Staff
+{
+    [EnumClass(
+        TargetClassName = nameof(Staff), 
+        Namespace = "EnumClass.NestedEnum")]
+    public enum StaffType
+    {
+        Manager,
+        Programmer,
+        Tester,
+        CTO,
+        CEO,
+    }
+
+
+    public abstract void Speak();
+
+    public partial class ProgrammerEnumValue
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("0110011011");
+        }
+    }
+
+    public partial class ManagerEnumValue
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Deadline was yesterday!");
+        }
+    }
+
+    public partial class CTOEnumValue
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("We need microservices!");
+        }
+    }
+
+    public partial class CEOEnumValue
+    {
+        public override void Speak()
+        {
+            var old = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("EnumClass is the best product!!!");
+            Console.ForegroundColor = old;
+        }
+    }
+
+    public partial class TesterEnumValue
+    {
+        public override void Speak()
+        {
+            Console.WriteLine(string.Join(',', Enumerable.Range(0, 100).Select(_ => Random.Shared.Next().ToString())));
+        }
+    }
+}
